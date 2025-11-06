de12a32e86e1cd2e24ed44a32cf082428fd92120
navneeth-sortur <navneeth.sortur@gmail.com>
2025-11-06 19:31:34 +0530

    added alternate solutions to flatten a nested array

--- problems/16-flatten-array.js
+++ problems/16-flatten-array.js
@@ -8,8 +8,9 @@
  * Output: [1, 2, 3, 4, 5]
  *
  * Approaches:
- * 1️⃣ Recursive with reduce() (Preferred)
- * 2️⃣ Using flat(Infinity)
+ * 1 Iterative using stack (most optimized)
+ * 2 Recursive with reduce() (cleaner version)
+ * 3 Basic Recursive
  */
 
 /**
@@ -17,11 +18,43 @@
  * @returns {any[]} flattened array
  */
 function flattenArray(arr) {
-  return arr.reduce(
-    (acc, val) => acc.concat(Array.isArray(val) ? flattenArray(val) : val),
-    []
-  );
+  const stack = [...arr];
+  const result = [];
+
+  while (stack.length) {
+    const next = stack.pop();
+    if (Array.isArray(next)) {
+      stack.push(...next);
+    } else {
+      result.push(next);
+    }
+  }
+
+  return result.reverse(); // because stack reverses order
 }
+
+// function flattenReduce(arr) {
+//   return arr.reduce(
+//     (acc, val) => acc.concat(Array.isArray(val) ? flattenReduce(val) : val),
+//     []
+//   );
+// }
+
+// console.log(flattenReduce([1, 2, 3, [4, 5, 6, [7, 8, [10, 11]]], 9]));
+
+// function flattenRecursive(arr) {
+//   let result = [];
+//   for (let item of arr) {
+//     if (Array.isArray(item)) {
+//       result = result.concat(flattenRecursive(item));
+//     } else {
+//       result.push(item);
+//     }
+//   }
+//   return result;
+// }
+
+// console.log(flattenRecursive([1, 2, 3, [4, 5, 6, [7, 8, [10, 11]]], 9]));
 
 module.exports = { flattenArray };
 
